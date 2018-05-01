e3d5ee36b071b17ed7a90bcef97dc60dba369f23
iknepper <iknepper@hotmail.com>
2018-05-01 17:20:29 -0400

    packing party!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("index.js running");
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1IjoiaWtuZXBwZXIiLCJhIjoiY2pnbzVrbDhqMDl4dzMzcXEzemlqb21reiJ9.-q-oaCGe1WKcPIG9vi48GQ";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
